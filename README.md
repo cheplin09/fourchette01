f583ee8292f5e438a483b026b124b804215c5fe7
cheplin09 <grigor.leemann@gmail.com>
2022-03-24 15:28:16 +0000

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # fourchette01
 Devinez un nombre entre 1 et 10
+
+# Super jeu de devinette d'un nombre !
+
+Je pense (ordinateur) à une nombre et vous devez le deviner ;-)
+
+Pour le démarrer, faite simplement:
+
+```
+python fourchette1.py
+```
+
+
+
+
---
+++ fourchette1.py
@@ -0,0 +1,24 @@
+import time
+import random
+
+print('\nJeu de la fourchette !\nVous devez deviner un nombre entre 1 et 10')
+
+y = random.randint(1, 10)
+#print(str(y))
+
+time.sleep(3)
+print('\nEntrer un nombre:')
+x = int(input())
+
+while (x != y):
+
+	if x < y :
+		print('trop petit') 
+	if x > y :
+		print('trop grand') 
+	time.sleep(3)
+	print('\nEntrer un nombre:')
+	x = int(input())
+
+print('\n *************** Bravo ! **************\n') 
+
